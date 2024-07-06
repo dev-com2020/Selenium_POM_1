a47edcb4306c7a089e24f36ef05685bc3fa18384
DEV-COM <kwp@tlen.pl>
2024-07-06 16:14:47 +0200

    pierwsze testy 2

---
+++ src/test/java/LifecycleJUnit5Test.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.*;
+
+public class LifecycleJUnit5Test {
+    @BeforeAll
+    static void setupAll() {
+        System.out.println("Setup ALL TESTS");
+    }
+
+    @BeforeEach
+    void setup(){
+        System.out.println("Setup EACH TEST");
+    }
+    @Test
+    void testOne(){
+        System.out.println("TEST 1");
+    }
+    @Test
+    void testTwo(){
+        System.out.println("TEST 2");
+    }
+    @AfterEach
+    void teardown(){
+        System.out.println("Teardown EACH TEST");
+    }
+    @AfterAll
+    static void teardownAll(){
+        System.out.println("Teardown ALL TESTS");
+    }
+
+}

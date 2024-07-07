5c6c0588449e6e21330736e39ab2fbadf5239519
DEV-COM <kwp@tlen.pl>
2024-07-07 10:43:54 +0200

    niedziela 3

---
+++ src/test/java/TunningDisplayInRepeatedTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
+
+public class TunningDisplayInRepeatedTest {
+
+    @RepeatedTest(value = 2,name = "{displayName} {currentRepetition} / {totalRepetitions}")
+    @DisplayName("Powtórka!")
+    void customDisplayName(TestInfo testInfo){
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @RepeatedTest(value = 2,name = RepeatedTest.LONG_DISPLAY_NAME)
+    @DisplayName("Test który używa long name")
+    void customDisplayNameWithLongPattern(TestInfo testInfo){
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @RepeatedTest(value = 2,name = RepeatedTest.SHORT_DISPLAY_NAME)
+    @DisplayName("Test który używa short name")
+    void customDisplayNameWithShortPattern(TestInfo testInfo){
+        System.out.println(testInfo.getDisplayName());
+    }
+}

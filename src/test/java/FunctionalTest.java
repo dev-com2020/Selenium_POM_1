120f066937b82af80e8c3034cca4e058e2d78f55
DEV-COM <kwp@tlen.pl>
2024-07-07 09:35:27 +0200

    niedziela 1

---
+++ src/test/java/FunctionalTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("non-functional")
+public class FunctionalTest {
+    @Test
+    void testOne(){
+        System.out.println("Funkcjonalny Test 1");
+    }
+    @Test
+    void testTwo(){
+        System.out.println("Funkcjonalny Test 2");
+    }
+}
---
+++ src/test/java/Os.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public enum Os {
+    WINDOWS, MAC, LINUX, OTHER;
+
+
+    public static Os determine() {
+        Os out = OTHER;
+        String myOs = System.getProperty("os.name").toLowerCase();
+        if (myOs.contains("win")) {
+            out = WINDOWS;
+        } else if (myOs.contains("mac")) {
+            out = MAC;
+        } else if (myOs.contains("nux")) {
+            out = LINUX;
+        }
+        return out;
+    }
+}

a3d31263bdb802a16fa493a61f94481bc834bd0c
DEV-COM <kwp@tlen.pl>
2024-07-07 16:26:25 +0200

    niedziela 15

---
+++ src/test/java/com/example/BasicSeleniumTest.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertTrue;
+
+public class BasicSeleniumTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp(){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    public void tearDown(){
+        if (driver != null){
+            driver.quit();
+        }
+    }
+
+    private ExpectedCondition<Boolean> pageTitleStartWith(final String searchString) {
+        return webDriver -> webDriver.getTitle().toLowerCase().contains(searchString.toLowerCase());
+    }
+
+    private void storeExampleThatSearchFor(final String searchString){
+        driver.get("http://demo-store.seleniumacademy.com/");
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.click();
+        searchField.sendKeys(searchString.toLowerCase());
+        searchField.submit();
+        wait.until(pageTitleStartWith(searchString));
+        assertTrue("The page title does not start with thee search string", driver.getTitle().toLowerCase().contains(searchString.toLowerCase()));
+    }
+    @Test
+    public void searchOneExample() {
+        storeExampleThatSearchFor("CHELSEA");
+    }
+    @Test
+    public void searchSecondExample() {
+        storeExampleThatSearchFor("ELIZABETH");
+    }
+}
